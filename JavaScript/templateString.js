deb88aa5c3a75d77d84ba6003ee7e72077f80514
Leandro da Silva <58789409+leandromsilva@users.noreply.github.com>
2021-03-31 21:46:03 -0300

    Update

--- JavaScript/templateString.js
+++ JavaScript/templateString.js
@@ -5,3 +5,6 @@ console.log(concatenacao, template)
 
 // expressão
 console.log(`1 + 1 = ${1 + 1}`)
+
+const up = text => text.toUpperCase() // criação de uma função que deixa todas as letras maiúsculas
+console.log(`Ei... ${up(`cuidado`)}!`)
