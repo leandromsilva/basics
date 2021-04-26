80a82471f2d0d6a5bb7cb79d434b084be7ac2bd5
Leo <lmrsilva@outlook.com>
2021-04-26 10:23:38 -0300

    Conceito de Hoisting

---
+++ JavaScript/hoisting.js
@@ -0,0 +1,10 @@
+console.log("a =", a)
+
+var a = 2 // o efeito holsting faz uma variáviel declarada com var ir para cima, não causando erros mas somente como indefinida
+
+console.log("a =", a)
+
+/*console.log("b =", b) 
+
+ let b = 2  // let não permite o holsting */
+
