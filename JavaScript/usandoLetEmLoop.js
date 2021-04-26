862e9dd6d42e28bc0e10dc5ace0ea377659e031a
Leo <lmrsilva@outlook.com>
2021-04-26 10:15:26 -0300

    Usando Let em Loop

--- JavaScript/usandoLetEmLoop.js
+++ JavaScript/usandoLetEmLoop.js
@@ -2,4 +2,16 @@ for(let i = 0; i < 10; i++){
     console.log(i)
 }
 
-console.log("i =", i) // erro, let só pode ser acessado dentro do escopo
+console.log()
+
+// console.log("i =", i) // erro, let só pode ser acessado dentro do escopo
+
+const funcs = []
+for(let i = 0; i < 10; i++){
+    funcs.push(function(){
+        console.log(i)
+    })
+}
+
+funcs[2] () // no exemplo de let as duas apontam para o valor correto
+funcs[8] ()
