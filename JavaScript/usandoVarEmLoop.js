5dbe6963753b3e39702ef71ddb8d52424bfd515d
Leo <lmrsilva@outlook.com>
2021-04-26 10:12:12 -0300

    Usando Var em Loop

--- JavaScript/usandoVarEmLoop.js
+++ JavaScript/usandoVarEmLoop.js
@@ -4,3 +4,13 @@ for(var i = 0; i < 10; i++)
 }
 
 console.log("i =", i)
+
+const funcs = []
+for(var i = 0; i < 10; i++){
+    funcs.push(function(){
+        console.log(i)
+    })
+}
+
+funcs[2] ()
+funcs[8] ()
